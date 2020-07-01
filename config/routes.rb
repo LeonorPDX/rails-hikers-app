e67cfd7ba7c429c072c6f650e782c9c8508cd710
LeonorPDX <leonoracolbert@gmail.com>
2020-07-01 08:41:56 -0700

    Specify custom controllers for Devise, rescope login and signup routes to be more user friendly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: "callbacks"}
+  devise_scope :user do 
+      get 'login', to: 'devise/sessions#new'
+  end
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
