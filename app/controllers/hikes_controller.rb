37e5a0e13e64d492e0602e4b5f0b514ed61f4fbe
LeonorPDX <leonoracolbert@gmail.com>
2020-07-03 12:03:21 -0700

    Added index and show pages to trailheads and hikes

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,11 @@
 class HikesController < ApplicationController
+
+    def index
+        @hikes = Hike.all 
+    end
+
+    def show
+        @hike = Hike.find_by(id: params[:id])
+    end
+    
 end
--- app/controllers/trailheads_controller.rb
+++ app/controllers/trailheads_controller.rb
@@ -1,2 +1,12 @@
 class TrailheadsController < ApplicationController
+
+    def index
+        @trailheads = Trailhead.all
+    end
+
+    def show
+        @trailhead = Trailhead.find_by(id: params[:id])
+        @hikes = @trailhead.hikes
+    end
+
 end
