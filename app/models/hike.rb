fbd5176769c0b055b4cf3dd647269ccc0af4f334
LeonorPDX <leonoracolbert@gmail.com>
2020-07-05 08:52:29 -0700

    Added method to show boolean attr of hikes in model and display on view, plus user created by line on hike show view

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -10,4 +10,39 @@ class Hike < ApplicationRecord
     validates :hike_type, presence: true
     validates :description, length: { in: 50..6000}
 
+    def boolean_display
+        display_text = nil
+        arr = []
+
+        if self.waterfalls == true
+            arr << "Waterfalls"
+        end
+
+        if self.wildflowers == true
+            arr << "Wildflowers"
+        end
+
+        if self.mountains == true
+            arr << "Mountains"
+        end
+
+        if self.beaches == true
+            arr << "Beaches"
+        end
+
+        if self.family_friendly == true
+            arr << "Family-friendly"
+        end
+
+        if self.dog_friendly == true
+            arr << "Dog-friendly"
+        end
+
+        unless arr.empty?
+            display_text = arr.join(", ")
+        end
+
+        display_text
+    end
+
 end
