3fd9be23d53c387cc2f2231902fa56e68a086de1
LeonorPDX <leonoracolbert@gmail.com>
2020-07-05 14:04:57 -0700

    Added check in button and flash notice if user tries to check in twice in the same day at the same trailhead

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,2 +1,14 @@
 class CheckInsController < ApplicationController
+    def create
+        ch = CheckIn.new
+        ch.user = current_user
+        ch.check_in_date = Date.today
+        ch.trailhead_id = params[:trailhead_id]
+        if ch.save
+            redirect_to trailhead_path(ch.trailhead)
+        else
+            flash.notice = "Users can only check in once per day at a trailhead."
+            redirect_to trailhead_path(ch.trailhead)
+        end
+    end
 end
--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -2,7 +2,7 @@ class CheckIn < ApplicationRecord
     belongs_to :user
     belongs_to :trailhead
 
-    validates :check_in_date, presence: true, uniqueness: { scope: [:trailhead_id, :user_id], message: "can only be used once per day, per user, per trailhead." }
+    validates :check_in_date, presence: true, uniqueness: { scope: [:trailhead_id, :user_id] }
 
     def format_date
         self.check_in_date.strftime("%B %d, %Y")
