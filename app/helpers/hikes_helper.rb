7bacdb8d4c16b86805e9aac6bdf8a5ef92e24bcc
LeonorPDX <leonoracolbert@gmail.com>
2020-07-07 11:36:25 -0700

    Removed unused views and view directories, removed unused helper modules

--- app/helpers/hikes_helper.rb
+++
@@ -1,9 +0,0 @@
-module HikesHelper
-    def trailhead_id_field(hike)
-        if hike.trailhead.nil?
-          select_tag "hike[trailhead_id]", options_from_collection_for_select(Trailhead.all, :id, :name)
-        else
-          hidden_field_tag "hike[trailhead_id]", hike.trailhead_id
-        end
-    end
-end
