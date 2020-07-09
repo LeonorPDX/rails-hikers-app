02739570325ec2d75f6514e0b9e38755fdf0b03b
LeonorPDX <leonoracolbert@gmail.com>
2020-07-08 19:06:37 -0700

    Moved boolean display method from hike model to helper module

--- app/helpers/hikes_helper.rb
+++ app/helpers/hikes_helper.rb
@@ -1,3 +1,38 @@
 module HikesHelper
-  #add boolean display from model
+
+  def boolean_display(hike)
+    display_text = nil
+    arr = []
+
+    if hike.waterfalls == true
+        arr << "Waterfalls"
+    end
+
+    if hike.wildflowers == true
+        arr << "Wildflowers"
+    end
+
+    if hike.mountains == true
+        arr << "Mountains"
+    end
+
+    if hike.beaches == true
+        arr << "Beaches"
+    end
+
+    if hike.family_friendly == true
+        arr << "Family-friendly"
+    end
+
+    if hike.dog_friendly == true
+        arr << "Dog-friendly"
+    end
+
+    unless arr.empty?
+        display_text = arr.join(", ")
+    end
+
+    display_text
+  end
+  
 end
--- app/models/hike.rb
+++ app/models/hike.rb
@@ -37,40 +37,5 @@ class Hike < ApplicationRecord
     def self.waterfalls
         where(waterfalls: true)
     end
-    
-    def boolean_display
-        display_text = nil
-        arr = []
-
-        if self.waterfalls == true
-            arr << "Waterfalls"
-        end
-
-        if self.wildflowers == true
-            arr << "Wildflowers"
-        end
-
-        if self.mountains == true
-            arr << "Mountains"
-        end
-
-        if self.beaches == true
-            arr << "Beaches"
-        end
-
-        if self.family_friendly == true
-            arr << "Family-friendly"
-        end
-
-        if self.dog_friendly == true
-            arr << "Dog-friendly"
-        end
-
-        unless arr.empty?
-            display_text = arr.join(", ")
-        end
-
-        display_text
-    end
 
 end
