b770a92588b0066203af22831b624e6ebf33c38c
LeonorPDX <leonoracolbert@gmail.com>
2020-07-11 18:10:01 -0700

    More styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find_by(id: params[:id])
-        @hikes = @user.hikes
-        @trip_reports = @user.trip_reports
-        @check_ins = @user.check_ins
-        @trailheads = @user.trailheads
+        @hikes = @user.hikes.reverse
+        @trip_reports = @user.trip_reports.reverse
+        @check_ins = @user.check_ins.reverse
+        @trailheads = @user.trailheads.reverse
     end
     
 end
